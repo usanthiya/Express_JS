03d3e8a5f45004f857bf6e0212ff0de020f48438
santhiyaudhaya <santhiya@datumailabs.com>
2025-08-10 11:10:22 +0530

    express server

---
+++ web_server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = 3500;
+
+// Define a GET route for the root URL '/' that sends a plain text response
+app.get('/',(req, res)=>{
+  res.send("Hello World!");
+})
+
+// Define a GET route for '/index.html' that sends the index.html file located in the 'views' folder
+app.get('/index.html',(req, res)=>{
+  res.sendFile(path.join(__dirname,'views','index.html'));
+})
+
+// Define a GET route for '/new-page.html' that sends the new-page.html file located in the 'views' folder
+app.get('/new-page.html',(req, res)=>{
+  res.sendFile(path.join(__dirname,'views','new-page.html'));
+})
+
+// Start the server
+app.listen(PORT,()=>{
+    console.log(`Server is running on http://localhost:${PORT}`);
+})
