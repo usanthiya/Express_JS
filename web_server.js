fdff1f50b4e5f70370ef2af49a3592d3a499e50b
santhiyaudhaya <santhiya@datumailabs.com>
2025-09-06 10:13:54 +0530

    web server

--- web_server.js
+++ web_server.js
@@ -18,6 +18,10 @@ app.get('/new-page.html',(req, res)=>{
   res.sendFile(path.join(__dirname,'views','new-page.html'));
 })
 
+app.get('/old-page.html',(req,res)=>{
+  res.redirect('new-page.html')
+})
+
 // Start the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
