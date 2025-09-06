5ae6dfcca6607e56f289c8387db44e7951df80f8
santhiyaudhaya <santhiya@datumailabs.com>
2025-09-06 10:58:27 +0530

    next()

--- web_server.js
+++ web_server.js
@@ -22,6 +22,42 @@ app.get('/old-page.html',(req,res)=>{
   res.redirect('new-page.html')
 })
 
+// First middleware logs the request then calls next(), second handler sends the final response.
+app.get('/hello.html',(req, res, next)=>{
+    console.log("Trying to load hello.html page");
+    next();
+},(req,res)=>{
+    res.send("Hi, Hello");
+})
+
+// Executes 'one' → 'two' → 'three' in sequence for the /chain route.
+/**
+ * 
+one runs → logs "one", then calls next()
+two runs → logs "two", then calls next()
+three runs → logs "three", then sends 'Finished!'
+ */
+const one = (rq, res, next)=>{
+  console.log("one");
+  next();
+}
+
+const two = (rq, res, next)=>{
+  console.log("two");
+  next();
+}
+
+const three = (rq, res)=>{
+  console.log("three");
+  res.send('Finished!')
+}
+
+app.get('/chain',[one, two, three]);
+
+app.use((req,res)=>{
+  res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+})
+
 // Start the server
 app.listen(PORT,()=>{
     console.log(`Server is running on http://localhost:${PORT}`);
